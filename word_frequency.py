ae0cc4595efae3290384a8b21e743682373cd507
Joel Thompson <chameleonsrock@live.com>
2015-01-15 09:32:23 -0500

    Removed blank spaces from the dictionary count, removed 2 redundant lists

--- word_frequency.py
+++ word_frequency.py
@@ -1,11 +1,8 @@
-banned_chars = [".", ",", '"', "'", "?", "!", "@", "&", "%",
-                "$", "' ", " - ", " '"]
-spaced_banned = ["' ", " - ", " '",]
-
 def clean_it(word):
     """Strips unwanted characters from the file."""
-    banned_chars = [".", ",", '"', "'", "?", "!", "@", "&", "%",
-                    "$", "' ", " - ", " '"]
+    banned_chars = [".", ",", '"', "'", "?",
+                    "!", "@", "&", "%","$",
+                    "' ", " - ", " '"]
     spaced_banned = ["' ", "-", " '", ' ']
 
     word = word.lower()
@@ -20,9 +17,8 @@ def clean_it(word):
     return word
 
 
-
 def word_frequency(file_name):
-    "Determines how many times a word occurs in a text file."""
+    """Determines how many times a word occurs in a text file."""
 
     with open(file_name) as text:
         file = text.readlines()
@@ -37,15 +33,19 @@ def word_frequency(file_name):
         for word in clean_list:
             final_list.append(clean_it(word))
 
+        final_list = [x for x in final_list if x.strip()]
+
         for word in final_list:
             if word in word_dict:
                 word_dict[word] += 1
             else:
                 word_dict[word] = 1
-    sorted_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-    top_20 = sorted_dict[1:20] # couldn't figure out how to leave spaces out of count, so I started at the next highest value
-    for key, value in top_20:
-        print("{}: {}".format(key, value))
+
+    sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    top_20 = sorted_list[0:20]
+
+    for item, count in top_20:
+        print("{}: {}".format(item, count))
 
 
 print(word_frequency("sample.txt"))
